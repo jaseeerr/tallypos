f0eb308bde645b2cd7949dbb21eb9f8da91a2371
jaseeerr <jaseerhasbulla01@gmail.com>
2025-12-11 19:25:58 +0400

    syncApiUpdates22

--- server/app.js
+++ server/app.js
@@ -21,8 +21,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: true, limit: "200mb" }));
+
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use("/uploads", express.static("uploads"));
