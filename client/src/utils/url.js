9587017f0cca62a6b724f6850cf9161e2e73ee4a
jaseeerr <jaseerhasbulla01@gmail.com>
2025-12-11 14:31:42 +0400

    first auth fix2222

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,6 +1,6 @@
-// export const API_BASE = "http://localhost:3063"; 
+export const API_BASE = "http://localhost:3063"; 
  
-export const API_BASE = "https://app.fancypalace.cloud/api"; 
+// export const API_BASE = "https://app.fancypalace.cloud/api"; 
 
 
 // export const API_BASE = "https://tallypos.syntegrateitsolutions.com/api"; 
