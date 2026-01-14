9baeaca8f3b471b03f290468f35641effd38ba46
jaseeerr <jaseerhasbulla01@gmail.com>
2026-01-14 18:32:26 +0400

    homepage dashboard22222222222222222222

--- client/src/components/Scanner.jsx
+++ client/src/components/Scanner.jsx
@@ -155,7 +155,7 @@ function Scanner() {
       </button>
 
       {/* SCANNER MODAL */}
-      {scannerOpen && (
+      {scannerOpen && !isFlutterApp && (
         <div className="fixed inset-0 z-50 bg-black/70 flex items-center justify-center p-4">
           <div className="bg-white w-full max-w-3xl rounded-2xl shadow-2xl overflow-hidden">
             {/* Header */}
